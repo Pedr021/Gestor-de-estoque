39c397a2a1af9e24820aed0005891a2d778bd389
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2022-06-13 08:28:41 -0300

    Add files via upload

---
+++ Gestor de Estoque Orientado/Gestor de estoque/Ebook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    [System.Serializable]
+    class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook (string nome, float preco,string autor)
+        {
+            this.nome = nome;
+            this.autor = autor;
+            this.preco = preco;
+            
+        }
+
+        public void AdicionarEntrada()
+        {
+           
+        }
+
+        public void AdicionarSaida()
+        {
+            
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Nome: {preco}");
+            Console.WriteLine($"Vendas  : {vendas}");
+            Console.WriteLine("============================");
+        }
+    }
+}
---
+++ Gestor de Estoque Orientado/Projeto3/Ebook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    [System.Serializable]
+    class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook (string nome, float preco,string autor)
+        {
+            this.nome = nome;
+            this.autor = autor;
+            this.preco = preco;
+            
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine("Não é possível dar entrada no estoque de um E-book devido ser um produto digita");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar vendas no E-book {nome}");
+            Console.WriteLine("Digite a Qtd. de vendas que você quer dar entrada:");
+            int entrada = int.Parse(Console.ReadLine());
+            vendas += entrada;
+            Console.WriteLine("Saída registrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Nome: {preco}");
+            Console.WriteLine($"Vendas  : {vendas}");
+            Console.WriteLine("============================");
+        }
+    }
+}
