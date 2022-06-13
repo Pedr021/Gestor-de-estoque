6249a7067b5de8e2dcc6aefaf7c20f68cd66baf9
Pedro Canuto <104801029+Pedr021@users.noreply.github.com>
2022-06-13 08:25:24 -0300

    Add files via upload

---
+++ Gestor de estoque/ProdutoFisico.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    [System.Serializable]
+     class ProdutoFisico : Produto, IEstoque
+    {
+        public float frete;
+        private int estoque;
+        
+        public ProdutoFisico(string nome , float preco , float frete )
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.frete = frete;
+            
+        }
+
+        public void AdicionarEntrada()
+        {
+           
+        }
+
+        public void AdicionarSaida()
+        {
+           
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Frete: {frete}");
+            Console.WriteLine($"Nome: {preco}");
+            Console.WriteLine($"Estoque : {estoque}");
+            Console.WriteLine("============================");
+        }
+    }
+}
---
+++ Gestor de estoque/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    class Program
+    {
+
+        static List<IEstoque> produtos = new List<IEstoque>();
+        enum Menu { Listar = 1, Adicionar, Remover , Entrada , Saida, Sair} 
+        static void Main(string[] args)
+        {
+            Carregar();
+            bool escolheuSair = false;
+            while (escolheuSair == false)
+            {
+                Console.WriteLine("Sistema de Estoque");
+                Console.WriteLine("1-Listar\n2-Adicionar\n3-Remover\n4-Registrar Entrada\n5-Registrar Saida\n6-Sair");
+
+                string opStr = Console.ReadLine();
+                int opInt = int.Parse(opStr);
+
+                if (opInt > 0 && opInt < 7)
+                {
+                    Menu escolha = (Menu)opInt;
+
+                    switch (escolha)
+                    {
+                        case Menu.Listar:
+                            Listagem();
+                            break;
+                        case Menu.Adicionar:
+                            Cadastro();
+                            break;
+                        case Menu.Remover:
+                            Remover();
+                            break;
+                        case Menu.Entrada:
+                            Entrada();
+                            break;
+                        case Menu.Saida:
+                            Saida();
+                            break;
+                        case Menu.Sair:
+                            escolheuSair |= true;
+                            break;
+
+                    }
+                } 
+                else
+                {
+                    escolheuSair = true;
+                }
+
+
+                Console.Clear();
+
+            }
+
+        }
+
+        static void Listagem()
+        {
+            Console.WriteLine("Lista de produtos");
+            foreach (IEstoque produto in produtos)
+            {
+                produto.Exibir();
+            }
+            Console.ReadLine();
+        }
+
+        static void Remover()
+        {
+            Listagem();
+            Console.WriteLine("Digite o ID do elemento que voce quer remover:");
+            int id = int.Parse(Console.ReadLine());
+            if (id >=0 && id< produtos.Count)
+            {
+                produtos.RemoveAt(id);
+                Salvar();
+            } 
+
+
+
+        }
+        static void Entrada()
+        {
+            Listagem();
+            Console.WriteLine("Digite o ID do elemento que voce quer dar entrada:");
+            int id = int.Parse(Console.ReadLine());
+            if (id >= 0 && id < produtos.Count)
+            {
+                produtos[id].AdicionarEntrada();
+                Salvar();
+            }
+        }
+        static void Saida()
+        {
+            Listagem();
+            Console.WriteLine("Digite o ID do elemento que voce quer dar baixa:");
+            int id = int.Parse(Console.ReadLine());
+            if (id >= 0 && id < produtos.Count)
+            {
+                produtos[id].AdicionarSaida();
+                Salvar();
+            }
+        }
+
+
+
+
+        static void Cadastro()
+        {
+            Console.WriteLine("Cadastro de Produtos:");
+            Console.WriteLine("1-Produto Fisico\n2-Ebook\n3-Curso");
+            string opStr = Console.ReadLine();
+            int escolhaInt = int.Parse(opStr);
+            switch (escolhaInt)
+            {
+                case 1:
+                    CadastriPFisico();
+                    break;
+                         case 2:
+                    CadastrarEbook();
+                    break;
+                case 3:
+                    CadastrarCurso();
+                    break;
+            }
+        }
+        static void CadastriPFisico()
+        {
+            Console.WriteLine("Cadastrando produto fisico :");
+            Console.WriteLine("Nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            float preco = float.Parse(Console.ReadLine());
+            Console.WriteLine("Frete:");
+            float frete = float.Parse(Console.ReadLine());
+            ProdutoFisico pf =new ProdutoFisico(nome, preco, frete);
+            produtos.Add(pf);
+            Salvar();
+        }
+        static void CadastrarEbook ()
+        {
+            Console.WriteLine("Cadastrando produto fisico :");
+            Console.WriteLine("Nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            float preco = float.Parse(Console.ReadLine());
+            Console.WriteLine("Autor:");
+            string autor = Console.ReadLine();
+
+            Ebook eb = new Ebook(nome, preco, autor);
+            produtos.Add(eb);
+            Salvar();
+
+        }
+        static void CadastrarCurso()
+        {
+            Console.WriteLine("Cadastrando produto fisico :");
+            Console.WriteLine("Nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            float preco = float.Parse(Console.ReadLine());
+            Console.WriteLine("Autor:");
+            string autor = Console.ReadLine();
+
+            Curso cs = new Curso(nome, preco, autor);
+            produtos.Add(cs);
+            Salvar();
+
+        }
+
+        static void Salvar()
+        {
+            FileStream stream = new FileStream("produtos.dat", FileMode.OpenOrCreate);
+            BinaryFormatter encoder = new BinaryFormatter();
+
+            encoder.Serialize(stream, produtos);
+
+            stream.Close();
+
+        }
+        static void Carregar()
+        {
+            FileStream stream = new FileStream("produtos.dat", FileMode.OpenOrCreate);
+            BinaryFormatter encoder = new BinaryFormatter();
+
+            try
+            {
+                produtos = (List<IEstoque>)encoder.Deserialize(stream);
+
+                if (produtos == null)
+                {
+                    produtos = new List<IEstoque>();
+                }        
+            }
+            catch(Exception e )
+            {
+                produtos = new List<IEstoque>();
+            }
+            stream.Close();
+           
+
+        }
+        
+      
+
+        
+    }  
+}
